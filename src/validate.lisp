f4b7cf4a8c05a14ef5ebfe2a4f60285038498b93
Matt Novenstern <fisxoj@gmail.com>
2016-01-26 15:55:15 -0500

    Change it up.  New idea

--- src/validate.lisp
+++ src/validate.lisp
@@ -1,18 +1,37 @@
 (in-package :cl-user)
 (defpackage validate
-  (:use :cl)
+  (:use :iterate :cl)
+  (:nicknames #:v)
   (:export #:parse
-           #:validate
-           #:<parse-error>))
+           #:schema
+           #:<parse-error>
+           #:str
+           #:int
+           #:bool
+           #:email))
 (in-package :validate)
 
-(defun validate (data &key (as :plist))
 
-  )
+(defun schema (schema data &key (from :plist) (as :plist) allow-other-fields)
+  (declare (optimize debug))
+  (let ((schema (funcall
+                 (ecase from
+                   (:plist #'alexandria:plist-alist)
+                   (:alist #'identity)
+                   (:hash-table #'alexandria:hash-table-alist))
+                 schema)))
 
-(defun validate-field (object definiton)
+    (funcall
+     (ecase as
+       (:plist #'alexandria:alist-plist)
+       (:alist #'identity)
+       (:hash-table #'alexandria:alist-hash-table))
 
-  )
+     ;; Run validation function on every entry in the schema
+     (iterate (for (field . validation) in schema)
+              (for value = (getf data field))
+
+              (collect (cons field (funcall validation value)))))))
 
 
 (define-condition <parse-error> (simple-error)
@@ -20,24 +39,46 @@
    (type :initarg :type))
   (:report (lambda (condition stream)
              (with-slots (value type) condition
-                 (format stream "Value ~a is not of type ~A" value type))))
+               (format stream "Value ~a is not of type ~A" value type))))
   (:documentation "An error for conditions where a submitted value cannot be interpreted as the given type."))
 
-;; https://github.com/alecthomas/voluptuous/blob/master/voluptuous.py#L1265
-(defgeneric parse (value type)
-  (:method (value type)
-    value)
+(define-condition <validation-error> (simple-error)
+  ((value :initarg :value)
+   (rule :initarg :rule))
+  (:report (lambda (condition stream)
+             (with-slots (rule value) condition
+                 (format stream "Value ~a didn't satisfy condition ~S" value rule))))
+  (:documentation "Error to signal a validation condition wasn't met.
+  e.g. Value 'a' didn't satisfy contition 'length at least 3 characters"))
 
-  (:method (value (type (eql 'integer)))
-    (handler-case
+(defun int (value)
+  (handler-case
         (parse-integer value)
       (parse-error (e)
         (declare (ignore e))
         (error '<parse-error> :type type :value value))))
 
-  (:method (value (type (eql 'boolean)))
-    (let ((downcased-value (string-downcase value)))
+;; https://github.com/alecthomas/voluptuous/blob/master/voluptuous.py#L1265
+(defun bool (value)
+  (let ((downcased-value (string-downcase value)))
       (cond
         ((member downcased-value '("y" "yes" "t" "true"  "on"  "enable" ) :test #'string=) t)
         ((member downcased-value '("n" "no"  "f" "false" "off" "disable") :test #'string=) nil)
-        (t (error '<parse-error>  :type type :value value))))))
+        (t (error '<parse-error>  :type type :value value)))))
+
+(defun str (value &key min-length max-length)
+  (when min-length
+    (unless (> (length value) min-length)
+      (error '<validation-error> :rule (format nil "length must be > ~d" min-length))))
+  (when max-length
+    (unless (< (length value) max-length)
+      (error '<validation-error> :rule (format nil "length must be < ~d" max-length))))
+
+  value)
+
+(defun email (value)
+  (unless (ppcre:scan ".+@.+\..+" value)
+    (error '<validation-error>
+              :rule (format nil "string doesn't contain an email address")
+              :value value))
+  value)
--- t/validate.lisp
+++ t/validate.lisp
@@ -1,19 +1,36 @@
 (in-package :cl-user)
 (defpackage validate-test
   (:use :cl
-        :validate
         :prove))
 (in-package :validate-test)
 
 (plan nil)
 
 ;; Test parsing
-(is (validate:parse "a string" 'string) "a string")
-(is (validate:parse "12" 'integer) 12)
-(is-error (validate:parse "a pizza" 'integer)
-          'validate:<parse-error>
-          "raise a parse error on strings that aren't integers")
+;; (is (validate:parse "a string" 'string) "a string")
+;; (is (validate:parse "12" 'integer) 12)
+;; (is-error (validate:parse "a pizza" 'integer)
+;;           'validate:<parse-error>
+;;           "raise a parse error on strings that aren't integers")
+;; (is (validate:parse "true" 'boolean) t)
+;; (is (validate:parse "no" 'boolean) nil)
+;; (is (validate:parse "y" 'boolean) t)
+;; (is (validate:parse "disable" 'boolean) nil)
 
 ;; Test schema validation
+
+(let ((schema (list
+               :name (lambda (value) (v:str value :min-length 1))
+               :age  #'v:int
+               :has-dog #'v:bool))
+      (data (list
+             :name    "matt"
+             :age     "27"
+             :has-dog "no")))
+
+  (let ((validated (validate:schema schema data)))
+    (is (getf validated :name)    "matt")
+    (is (getf validated :age)     27)
+    (is (getf validated :has-dog) nil)))
 
 (finalize)
