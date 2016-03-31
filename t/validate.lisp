636a1063597d05694e0b662c5f8a9f8a72956927
Matt Novenstern <fisxoj@gmail.com>
2016-03-30 20:06:12 -0400

    Clean up tests

--- t/validate.lisp
+++ t/validate.lisp
@@ -9,47 +9,74 @@
 ;; Test parsing
 
 ;;; Email
-(is-error (v:email "me@here") 'v:<validation-error> "bad email throws an error")
+(subtest "Email"
+  (is (v:email "me@here.xyz") "me@here.xyz" "Valid emails are ok.")
+  (is-error (v:email "me@here") 'v:<validation-error> "Email without a TLD throws an error.")
+  (is-error (v:email "me@here.a") 'v:<validation-error> "Email with an invalid TLD throws an error.")
+  (is-error (v:email "mehere.xyz") 'v:<validation-error> "Email with no @ throws an error."))
 
 ;;; String
-(is-error (v:str "me" :min-length 3) 'v:<validation-error> "short string throws an error")
-(is (v:str "a string") "a string")
+(subtest "Strings"
+  (is-error (v:str "me" :min-length 3) 'v:<validation-error> "A too-short string throws an error.")
+  (is (v:str "a string") "a string" "Validated strings match the original string."))
 
 ;;; Integer
-(is (v:int "12") 12)
-(is-error (v:int "a pizza")
-          'validate:<parse-error>
-          "raise a parse error on strings that aren't integers")
+(subtest "Integers"
+  (is (v:int "12") 12 "An integer gets converted to a string.")
+  (is-error (v:int "a pizza")
+	    'validate:<parse-error>
+	    "A parse error is raised on strings that aren't integers."))
 
 ;;; Booleans
-(is (v:bool "true") t)
-(is (v:bool "no") nil)
-(is (v:bool "y") t)
-(is (v:bool "disable") nil)
-(is-error (v:bool "2")
-          'validate:<parse-error>
-          "raise a parse error on strings that aren't boolean values")
+(subtest "Boolean values"
+  (subtest "True/False"
+    (is (v:bool "true") t "'true'")
+    (is (v:bool "True") t "'True")
+    (is (v:bool "false") nil "'false'")
+    (is (v:bool "FALSE") nil "'FALSE'")
+    (is (v:bool "f") nil "'f'")
+    (is (v:bool "t") t "'t'"))
+
+  (subtest "Yes/No"
+    (is (v:bool "no") nil "'No'")
+    (is (v:bool "n") nil "'n")
+    (is (v:bool "Yes") t "'Yes'")
+    (is (v:bool "YES") t "'YES")
+    (is (v:bool "y") t "'y'"))
+
+  (subtest "On/Off"
+    (is (v:bool "on") t "'on'")
+    (is (v:bool "off") nil "'off'"))
+
+  (subtest "Enable/Disable"
+    (is (v:bool "disable") nil "'disable'")
+    (is (v:bool "enable") t "'enable'"))
+
+  (is-error (v:bool "2")
+	    'validate:<parse-error>
+	    "raise a parse error on strings that aren't boolean values"))
 
 ;; Test schema validation
+(subtest "Schemas"
+  (let ((schema '(:name    (v:str :min-length 1)
+		  :age     v:int
+		  :has-dog v:bool
+		  :email   v:email))
 
-(let ((schema `(:name    ,(lambda (value) (v:str value :min-length 1))
-                :age     v:int
-                :has-dog v:bool
-                :email   v:email))
-      (data '(
-             :name    "matt"
-             :age     "27"
-             :has-dog "no"
-             :email   "something@example.com")))
+	(data   '(:name    "matt"
+		  :age     "27"
+		  :has-dog "no"
+		  :email   "something@example.com")))
 
-  (v:with-validated-values (name
-                            age
-                            (dog has-dog) ;; Value bound to a different name
-                            email)
-      (schema data)
-    (is name     "matt")
-    (is age      27)
-    (is dog  nil)
-    (is email    "something@example.com")))
+    (v:with-validated-values (name
+			      age
+			      (dog has-dog) ;; Value bound to a different name
+			      email)
+	(schema data)
+
+      (is name     "matt")
+      (is age      27)
+      (is dog      nil)
+      (is email    "something@example.com"))))
 
 (finalize)
